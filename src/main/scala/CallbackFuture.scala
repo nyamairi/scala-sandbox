3cc29a1375202f4dd457ce956a533c4b856a3536
Nagi Yamairi <n.yamairi@gmail.com>
2016-10-26 11:07:22 +0900

    CallbackFuture を追加する

---
+++ src/main/scala/CallbackFuture.scala
@@ -0,0 +1,37 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Future, Promise}
+import scala.util.Random
+
+class CallbackSomething {
+  val random = new Random()
+
+  def doSomething(onSuccess: Int => Unit, onFailure: Throwable => Unit): Unit = {
+    val i = random.nextInt(10)
+    if (i < 5)
+      onSuccess(i)
+    else
+      onFailure(new RuntimeException(i.toString))
+  }
+}
+
+class FutureSomething {
+  val callbackSomething = new CallbackSomething
+
+  def doSomething(): Future[Int] = {
+    val promise = Promise[Int]
+    callbackSomething.doSomething(i => promise.success(i), t => promise.failure(t))
+    promise.future
+  }
+}
+
+object CallbackFuture extends App {
+  val futureSomething = new FutureSomething
+
+  val iFuture = futureSomething.doSomething()
+  val jFuture = futureSomething.doSomething()
+
+  for {
+    i <- iFuture
+    j <- jFuture
+  } yield println(s"$i, $j")
+}
